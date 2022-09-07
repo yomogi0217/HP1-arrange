f2bc31c81da43e0b085a8617f1080ae22a4befa7
Yomogi <royroy0217@gmail.com>
2022-09-07 14:11:34 +0900

    レスポンシブ対応

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$('.nav-icon').click(function () {
+  $(this).toggleClass('open');
+  $('.nav-contents').toggleClass('open');
+})
+
+// $('.menu').click(function () {
+//   $('.nav-contents').removeClass('open');
+// })
